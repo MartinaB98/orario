684c13a560b6f59f30e17306f0e03b7cb7a7be41
MartinaB98 <martina9890@hotmail.com>
2019-05-08 18:58:59 +0200

    mod home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,3 +8,32 @@ import { Component } from '@angular/core';
 export class HomePage {
 
 }
+const Obj={
+  "user1":[
+    {   
+  name:'ange',
+  age: 3,
+  food: 'pesce'
+    }
+],
+
+"user2":[
+  {   
+    name:'pp',
+    age: 5,
+    food: 'banana'
+      }
+]
+  
+};
+
+//tutto il json
+const ObjString = JSON.stringify(Obj);
+
+
+//ottiene solo il primo oggetto 'user1' presente nel json
+// const ObjString = JSON.stringify(Obj.user1);
+
+console.log(ObjString);
+
+console.log(JSON.parse(ObjString));
