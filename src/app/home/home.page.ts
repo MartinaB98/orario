3b0f7f8fb7c80a20ef499f6e539e96672d736be8
MartinaB98 <martina9890@hotmail.com>
2019-05-09 16:47:46 +0200

    bla

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,41 +1,75 @@
 import { Component } from '@angular/core';
-import {Http} from '@angular/http';
+// ionic cordova plugin add cordova-plugin-advanced-http
+//npm install @ionic-native/http
+import {HTTP} from '@ionic-native/http/ngx';
+import {HttpClient} from '@angular/common/http';
+//npm install --save @ionic/storage
+import {Storage} from '@ionic/Storage';
+
+
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
+
+constructor(
+  public http: HTTP,
+  public httpClient: HttpClient,
+  public globalData: GlobalDataService,
+  public storage: Storage
+) { }
+
+
+
 export class HomePage {
 
 }
-const Obj={
-  "user1":[
-    {   
-  name:'ange',
-  age: 3,
-  food: 'ananas'
-    }
-],
 
-"user2":[
-  {   
-    name:'pp',
-    age: 5,
-    food: 'mela'
-      }
-]
-  
-};
+// getJSONNative(url: string, body: any) {
+//   this.http.setHeader('*', 'Content-Type', 'application/json');
+//   this.http.setDataSerializer('json');
+//   return new Promise((resolve, reject) => {
+//       this.http.post(url, body, {}).then(
+//           (response) => {
+//               const dati = JSON.parse(response.data);
+//               GlobalDataService.log(0, url, response);
+//               resolve(dati);
+//           },
+//           (err) => {
+//               GlobalDataService.log(2, 'Error', err);
+//               reject(err);
+//           }).catch(exception => {
+//           GlobalDataService.log(2, 'Catch', exception);
+//           reject();
+//       });
+//   });
+// }
 
-//tutto il json
-const ObjString = JSON.stringify(Obj);
+
+getJSONAngular(url: string, body: any) {
+  return new Promise((resolve, reject) => {
+    this.httpClient.post(url,body)
+    .subscribe(
+       (response) => {
+        GlobalDataService.log( 0,url, response);
+        resolve(reponse);
+      },
+       (err) => {
+        GlobalDataService.log( 2, 'Error', err);
+        reject(err);
+      });
+  });
+}
 
 
-//ottiene solo il primo oggetto 'user1' presente nel json
-// const ObjString = JSON.stringify(Obj.user1);
 
-console.log(ObjString);
 
-console.log(JSON.parse(ObjString));
+
+
+
+
 
 
